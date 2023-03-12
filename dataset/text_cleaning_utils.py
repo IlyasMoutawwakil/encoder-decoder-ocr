8213649eef3d30ca90c07bd6395711888b854d00
IlyasMoutawwakil <ilyas.moutawwakil@student-cs.fr>
2023-03-12 19:16:45 +0100

    bundled text preprocessing in one function

--- dataset/text_cleaning_utils.py
+++ dataset/text_cleaning_utils.py
@@ -10,28 +10,27 @@ SPECIAL_CHARCTERS = """!"#$£€%§&½'°()*+,-./:;<=>?@[\]^_`{|}~“”‘’«
 ALL_CHARACTERS = NUMBERS + ALPHABET + SPECIAL_ALPHABET + SPECIAL_CHARCTERS
 
 
-def clean_line(text):
+def preprocess_line(text):
     # does the same as clean_df but for a single text
     text = re.sub(f"[^{ALL_CHARACTERS}]", '', text)
     text = re.sub('\s+', ' ', text)
     text = text.strip()
     return text
 
+def preprocess_paragraph(text):
+    lines = []
+    for line in text.split('\n'):
+        line = preprocess_line(line)
+        if len(line) > 0:
+            lines.append(line)  
+    return lines
 
 def preprocess_wikipedia_dataset(dataset):
     dataset = dataset.map(
-        lambda x: {'lines': x['text'].split('\n')},
+        lambda x: {
+            'lines': preprocess_paragraph(x['text'])
+        },
         remove_columns=['text'],
-        num_proc=4,
-    )
-    dataset = dataset.map(
-        lambda x: {'lines': [clean_line(line) for line in x['lines']]},
-        remove_columns=['lines'],
-        num_proc=4,
-    )
-    dataset = dataset.map(
-        lambda x: {'lines': [line for line in x['lines'] if len(line) > 0]},
-        remove_columns=['lines'],
         num_proc=4,
     )
 
