corrected masking in xtransformers and started training

modeling/decoder.py
@@ -22,7 +22,6 @@ class AutoregressiveDecoder(AutoregressiveWrapper):
                  eos_token_id=1,
                  pad_token_id=2,
 
-                 *args,
                  **kwargs):
 
         net = TransformerWrapper(
@@ -34,23 +33,36 @@ class AutoregressiveDecoder(AutoregressiveWrapper):
         super().__init__(
             net=net,
             pad_value=pad_token_id,
-            *args, **kwargs
+            **kwargs,
         )
 
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
 
-    def generate(self, seq_len, context, *args, **kwargs):
+    def forward(self, target, **kwargs):
+
+        return super().forward(
+            target,
+            **kwargs,
+        )
+
+    @torch.no_grad()
+    def generate(self, seq_len=None, context=None, **kwargs):
+
+        if seq_len is None:
+            seq_len = self.max_seq_len
+
+        if context is None:
+            raise ValueError('context must be provided to generate')
+
         start_tokens = torch.full((context.size(0), 1), self.bos_token_id)
+
         return super().generate(
-            start_tokens=start_tokens,
-            context=context,
-
             seq_len=seq_len,
+            start_tokens=start_tokens,
             eos_token=self.eos_token_id,
-
-            *args,
-            **kwargs
+            context=context,
+            **kwargs,
         )
 
 
@@ -65,12 +77,12 @@ if __name__ == '__main__':
         ff_glu=False,
         attn_on_attn=False,
         use_scalenorm=False,
-        rel_pos_bias=False
+        rel_pos_bias=False,
     )
 
     # auto regressive wrapper architecture config
-    num_tokens = 100
-    max_seq_len = 256
+    num_tokens = 256
+    max_seq_len = 128
 
     # auto regressive wrapper generation config
     bos_token_id = 0
@@ -89,12 +101,20 @@ if __name__ == '__main__':
         pad_token_id=pad_token_id,
     )
 
-    # mandatory generation inputs
+    # mandatory decoder inputs
     seq_len = 96
-    sample_context = torch.randn(2, 100, decoder_config['dim'])
+    sample_context = torch.randn(2, 320, 384)
+    sample_target = torch.randint(0, num_tokens, (2, seq_len))
+
+    # auto regressive wrapper forward pass
+    loss = decoder(
+        target=sample_target,
+        context=sample_context,
+    )
+    print(loss)
 
     # optional generation inputs
-    from x_transformers.autoregressive_wrapper import top_k, top_p, top_a
+    from x_transformers.autoregressive_wrapper import top_k
     optional_inputs = dict(
         filter_logits_fn=top_k,
         temperature=1.0,

modeling/lightning_base.py
@@ -1,18 +1,19 @@
+import random
 import pytorch_lightning as pl
 import evaluate
 import torch
-import timm
 
 CER = evaluate.load("cer")
 ACC = evaluate.load("accuracy")
 
 
 class LightningBase(pl.LightningModule):
-    def __init__(self, model, optimizer_config, scheduler_config, *args, **kwargs):
+    def __init__(self, tokenizer, model, optimizer_config, scheduler_config, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.tokenizer = tokenizer
         self.model = model
-        
+
         self.optimizer_config = optimizer_config
         self.scheduler_config = scheduler_config
 
@@ -30,16 +31,19 @@ class LightningBase(pl.LightningModule):
             **self.scheduler_config['params'],
         )
 
-        return [optimizer], [scheduler]
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
 
-    def forward(self, features, targets, mask=None):
+    def lr_scheduler_step(self, scheduler, *args, **kwargs):
+        scheduler.step(self.global_step)
+
+    def forward(self, features=None, target=None, mask=None):
         return self.model(
             features=features,
-            targets=targets,
+            target=target,
             mask=mask
         )
 
-    def generate(self, seq_len, features=None, context=None):
+    def generate(self, seq_len=None, features=None, context=None):
         return self.model.generate(
             seq_len=seq_len,
             features=features,
@@ -47,12 +51,12 @@ class LightningBase(pl.LightningModule):
         )
 
     def training_step(self, batch, batch_num):
-        features, (targets, masks) = batch
+        features = batch['features']
+        target = batch['target']
 
-        train_loss, context = self(
+        train_loss, _ = self(
             features=features,
-            targets=targets,
-            mask=masks
+            target=target,
         )
 
         self.log(
@@ -63,49 +67,40 @@ class LightningBase(pl.LightningModule):
         return train_loss
 
     def validation_step(self, batch, batch_num):
-        features, (targets, masks) = batch
+        features = batch['features']
+        target = batch['target']
 
         val_loss, context = self(
             features=features,
-            targets=targets,
-            mask=masks
+            target=target,
         )
 
-        seq_len = self.model.decoder.max_seq_len
         generated = self.generate(
-            seq_len=seq_len,
             context=context,
         )
 
-        # pad generated and targets to the same length
-        max_len = max(generated.shape[1], targets.shape[1])
-        generated = torch.nn.functional.pad(
-            input=generated,
-            pad=(0, max_len - generated.shape[1]),
-            value=self.model.decoder.pad_token_id
-        )
-
-        targets = torch.nn.functional.pad(
-            input=targets,
-            pad=(0, max_len - targets.shape[1]),
-            value=self.model.decoder.pad_token_id
-        )
+        val_loss = val_loss.item()
 
         val_acc = ACC.compute(
-            predictions=generated,
-            references=targets,
-        )
+            predictions=generated.flatten(),
+            references=target.flatten(),
+        )["accuracy"]
+
+        generated = self.tokenizer.batch_decode(
+            generated, skip_special_tokens=True)
+        target = self.tokenizer.batch_decode(
+            target, skip_special_tokens=True)
 
         val_cer = CER.compute(
-            predictions=self.tokenizer.decode(generated),
-            references=self.tokenizer.decode(targets),
+            predictions=generated,
+            references=target,
         )
 
         return {
             'features': features,
-            'targets': targets,
+            'target': target,
             'generated': generated,
-            
+
             'val_loss': val_loss,
             'val_acc': val_acc,
             'val_cer': val_cer,
@@ -113,13 +108,45 @@ class LightningBase(pl.LightningModule):
 
     def validation_epoch_end(self, outputs):
 
-        features = torch.cat([o['features'] for o in outputs], dim=0)
-        targets = torch.cat([o['targets'] for o in outputs], dim=0)
-        generated = torch.cat([o['generated'] for o in outputs], dim=0)
+        wrong_cases = []
+        right_cases = []
 
-        val_loss = torch.stack([o['val_loss'] for o in outputs]).mean()
-        val_acc = torch.stack([o['val_acc'] for o in outputs]).mean()
-        val_cer = torch.stack([o['val_cer'] for o in outputs]).mean()
+        for output in outputs:
+            features = output['features']
+            target = output['target']
+            generated = output['generated']
+
+            for i, (t, g) in enumerate(zip(target, generated)):
+                if t != g:
+                    wrong_cases.append((t, g))
+                else:
+                    right_cases.append(features[i])
+
+        if len(wrong_cases) > 9:
+            wrong_cases = random.sample(wrong_cases, 9)
+        else:
+            pass
+
+        if len(right_cases) > 9:
+            right_cases = random.sample(right_cases, 9)
+        else:
+            pass
+
+        custom_log = "## WRONG CASES:"
+        custom_log += '\n\n'
+        custom_log += '\n\n'.join(
+            [f"- Ground Truth: {w1}\n- Prediction:&emsp;&ensp;{w2}" for w1, w2 in wrong_cases])
+
+        self.logger.experiment.add_text(
+            tag='wrong-cases-{i}', text_string=custom_log, global_step=self.global_step)
+
+        for i, image in enumerate(right_cases):
+            self.logger.experiment.add_image(
+                tag=f"right-cases-{i}", img_tensor=image, global_step=self.global_step, dataformats="CHW")
+
+        val_loss = sum([o['val_loss'] for o in outputs]) / len(outputs)
+        val_acc = sum([o['val_acc'] for o in outputs]) / len(outputs)
+        val_cer = sum([o['val_cer'] for o in outputs]) / len(outputs)
 
         self.log(
             name='val_loss', value=val_loss, on_step=False,
@@ -244,18 +271,17 @@ if __name__ == '__main__':
 
     # create one batch
     features = torch.randn(2, 3, 128, 640)
-    targets = torch.randint(0, 100, (2, 256))
-    masks = None
+    target = torch.randint(0, 100, (2, 256))
 
     # forward pass
-    loss, context = lightning_model(features, targets, masks)
+    loss, context = lightning_model(features, target)
 
     # generate
     generated = lightning_model.generate(
         seq_len=256,
         context=context,
     )
-    
+
     print(loss)
     print(context.shape)
     print(generated.shape)

modeling/vision_encoder_decoder.py
@@ -3,18 +3,22 @@ import torch
 
 class VisionEncoderDecoder(torch.nn.Module):
     def __init__(self, encoder, decoder):
+
         super().__init__()
 
         self.encoder = encoder
         self.decoder = decoder
 
-    def forward(self, features, targets, mask=None):
-        context = self.encoder(features)
+    def forward(self, features, target, **kwargs):
+
+        context = self.encoder(
+            features=features
+        )
 
         loss = self.decoder(
-            targets,
-            mask=mask,
+            target=target,
             context=context,
+            **kwargs
         )
 
         return loss, context
@@ -22,23 +26,23 @@ class VisionEncoderDecoder(torch.nn.Module):
     @torch.no_grad()
     def generate(
             self,
-            seq_len,
+            seq_len=None,
             features=None,
             context=None,
-            *args,
             **kwargs
     ):
         if features is None and context is None:
             raise ValueError(
                 "Either features or context should be provided")
 
-        if context is None:
-            context = self.encoder(features)
+        elif context is None:
+            context = self.encoder(
+                features=features,
+            )
 
         generated = self.decoder.generate(
             seq_len=seq_len,
             context=context,
-            *args,
             **kwargs
         )
 
@@ -86,7 +90,7 @@ if __name__ == '__main__':
 
     # auto regressive wrapper architecture config
     num_tokens = 100
-    max_seq_len = 256
+    max_seq_len = 128
 
     # auto regressive wrapper generation config
     bos_token_id = 0
@@ -113,19 +117,19 @@ if __name__ == '__main__':
 
     # vision encoder decoder generation inputs
     seq_len = 96
-    features = torch.randn(2, channels, height, width)
-    targets = torch.randint(0, num_tokens, (2, seq_len))
+    sample_features = torch.randn(2, channels, height, width)
+    sample_target = torch.randint(0, num_tokens, (2, seq_len))
 
     # vision encoder decoder forward pass
-    loss, context = model(
-        features=features,
-        targets=targets
+    loss, _ = model(
+        features=sample_features,
+        target=sample_target,
     )
-    print(loss, context.shape)
+    print(loss)
 
     # vision encoder decoder generation pass
     generated = model.generate(
         seq_len=seq_len,
-        features=features
+        features=sample_features
     )
-    print(generated.shape, targets.shape)
+    print(generated.shape)