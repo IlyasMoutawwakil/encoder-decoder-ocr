279354d67d8f434c396e6feef046a24707a07b0c
IlyasMoutawwakil <ilyas.moutawwakil@student-cs.fr>
2023-03-19 01:22:19 +0100

    fully pytorch native decoder

--- modeling/decoder.py
+++ modeling/decoder.py
@@ -1,101 +1,235 @@
-from x_transformers import Decoder, TransformerWrapper, AutoregressiveWrapper, NonAutoregressiveWrapper
 import torch
 
+class AutoregressiveTransformerDecoder(torch.nn.Module):
+    def __init__(
+        self,
+        # decoder architecture config
+        dim=384,
+        heads=8,
+        dropout=0.1,
+        activation='gelu',
+        norm_first=False,
+        # decoder stack config
+        num_layers=4,
+        # language model head config
+        num_tokens=256,
+        max_seq_len=128,
+        # auto regression config
+        bos_token_id=0,
+        eos_token_id=1,
+        pad_token_id=2,
+    ):
 
-class AutoregressiveDecoder(AutoregressiveWrapper):
-    def __init__(self,
-                 decoder_config=dict(
-                     dim=384,
-                     depth=4,
-                     heads=8,
-                     cross_attend=True,  # enables cross attention using the context argument, default is True
-                     ff_glu=False,  # use Gated Linear Units in the feed forward layer, default is False
-                     attn_on_attn=False,  # use attention on attention, default is False
-                     use_scalenorm=False,  # use ScaleNorm instead of LayerNorm, default is False
-                     rel_pos_bias=False,  # use relative positional bias, default is False
-                 ),
+        super().__init__()
 
-                 num_tokens=100,
-                 max_seq_len=256,
+        self.dim = dim
+        self.heads = heads
+        self.dropout = dropout
+        self.activation = activation
+        self.norm_first = norm_first
 
-                 bos_token_id=0,
-                 eos_token_id=1,
-                 pad_token_id=2,
+        self.num_layers = num_layers
 
-                 **kwargs):
-
-        net = TransformerWrapper(
-            num_tokens=num_tokens,
-            max_seq_len=max_seq_len,
-            attn_layers=Decoder(**decoder_config)
-        )
-
-        super().__init__(
-            net=net,
-            pad_value=pad_token_id,
-            **kwargs,
-        )
+        self.num_tokens = num_tokens
+        self.max_seq_len = max_seq_len
 
         self.bos_token_id = bos_token_id
         self.eos_token_id = eos_token_id
+        self.pad_token_id = pad_token_id
 
-    def forward(self, target, **kwargs):
+        self.tok_embed = torch.nn.Embedding(num_tokens, dim)
+        self.pos_embed = torch.nn.Embedding(max_seq_len, dim)
+        self.embed_norm = torch.nn.LayerNorm(dim)
+        self.embed_drop = torch.nn.Dropout(dropout)
 
-        return super().forward(
-            target,
-            **kwargs,
-        )
+        self.decs_stak = torch.nn.ModuleList([
+            torch.nn.TransformerDecoderLayer(
+                d_model=dim,
+                nhead=heads,
+                dropout=dropout,
+                activation=activation,
+                norm_first=norm_first,
+                batch_first=True,
+            ) for _ in range(num_layers)
+        ])
+
+        self.lang_mod_head = torch.nn.Linear(dim, num_tokens, bias=False)
+
+        # some optimizations I found in the nanoGPT repo
+        self.lang_mod_head.weight = self.tok_embed.weight
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, torch.nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(
+        self,
+        input_tokens,
+        memory_logits=None,
+    ):
+
+        seq_len = input_tokens.size(1)
+
+        assert seq_len <= self.max_seq_len, \
+            f"Cannot forward sequence of length {seq_len}," \
+            f"when max sequence length is only {self.max_seq_len}"
+
+        if memory_logits is None:
+            # decoder only mode
+            memory_logits = x
+
+        positions = torch.arange(
+            start=0,
+            end=seq_len,
+            device=input_tokens.device,
+        ).unsqueeze(0)
+
+        t_e = self.tok_embed(input_tokens)
+        p_e = self.tok_embed(positions)
+        x = self.embed_norm(t_e + p_e)
+        x = self.embed_drop(x)
+
+        for dec_layer in self.decs_stak:
+            x = dec_layer(
+                tgt=x,
+                memory=memory_logits,
+                tgt_is_causal=True,
+                memory_is_causal=False,
+            )
+
+        if self.training:
+            # if we are training, we predict all the tokens
+            logits = self.lang_mod_head(x)
+        else:
+            # if we are in inference (generation),
+            # we only predict the last token
+            logits = self.lang_mod_head(x[:, [-1], :])
+
+        return logits
 
     @torch.no_grad()
-    def generate(self, seq_len=None, context=None, **kwargs):
+    def generate(
+        self,
+        seq_len=None,
+        start_tokens=None,
+        memory_logits=None,
+        temperature=1.,
+        top_k=None,
+    ):
+
+        assert self.training is False, \
+            "Generation is only available in inference mode"
+
+        if start_tokens is not None:
+            # start with the provided tokens
+            generated_tokens = start_tokens
+        elif memory_logits is not None:
+            # start with the bos token
+            batch_size = memory_logits.size(0)
+            generated_tokens = torch.full(
+                size=(batch_size, 1),
+                fill_value=self.bos_token_id,
+                device=memory_logits.device,
+            )
+        else:
+            raise ValueError(
+                "Either start tokens or memory logits must be provided"
+                "to infer batch size."
+            )
 
         if seq_len is None:
             seq_len = self.max_seq_len
 
-        if context is None:
-            raise ValueError('context must be provided to generate')
+        for _ in range(seq_len-1):
 
-        start_tokens = torch.full(
-            (context.size(0), 1),
-            fill_value=self.bos_token_id,
-            device=context.device
-        )
+            if memory_logits is None:
+                # decoder only mode
+                memory_logits = generated_tokens
 
-        return super().generate(
-            seq_len=seq_len,
-            start_tokens=start_tokens,
-            eos_token=self.eos_token_id,
-            context=context,
-            **kwargs,
-        )
+            # only need the last token predictions (damn autoregressive models)
+            logits = self(
+                generated_tokens,
+                memory_logits=memory_logits,
+            )
+
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                # only keep top k tokens
+                v, _ = torch.topk(logits, top_k, dim=-1)
+                logits[logits < v[:, [-1]]] = -float('Inf')
+
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+
+            # sample from the distribution
+            next_tokens = torch.multinomial(probs, num_samples=1)
+
+            # append the new tokens to the generated sequence
+            generated_tokens = torch.cat(
+                [generated_tokens, next_tokens], dim=1)
+
+            # check if sentences have ended
+            is_eos_tokens = generated_tokens == self.eos_token_id
+            if is_eos_tokens.any(dim=-1).all():
+                # mask/pad all tokens after the first eos or pad token
+                shifted_is_eos_tokens = torch.nn.functional.pad(
+                    is_eos_tokens,
+                    pad=(1, -1),
+                )
+                mask = shifted_is_eos_tokens.cumsum(dim=-1) >= 1
+                generated_tokens = generated_tokens.masked_fill(
+                    mask,
+                    value=self.pad_token_id
+                )
+
+                # pad all sequences to the max length for consistency
+                generated_tokens = torch.nn.functional.pad(
+                    generated_tokens,
+                    (0, seq_len - generated_tokens.size(1)),
+                    value=self.pad_token_id
+                )
+                break
+
+        return generated_tokens
 
 
 if __name__ == '__main__':
 
-    # decoder architecture config
-    decoder_config = dict(
-        dim=384,
-        depth=4,
-        heads=8,
-        cross_attend=True,
-        ff_glu=False,
-        attn_on_attn=False,
-        use_scalenorm=False,
-        rel_pos_bias=False,
-    )
+    # decoder config
+    dim = 384
+    heads = 8
+    dropout = 0.1
+    activation = 'gelu'
+    norm_first = False  # norm before or after attention
 
-    # auto regressive wrapper architecture config
+    # decoder stack config
+    num_layers = 4
+
+    # language model config
     num_tokens = 256
     max_seq_len = 128
 
-    # auto regressive wrapper generation config
+    # auto regression config
     bos_token_id = 0
     eos_token_id = 1
     pad_token_id = 2
 
-    # create decoder
-    decoder = AutoregressiveDecoder(
-        decoder_config=decoder_config,
+    # autoregressive decoder
+    autoregressive_decoder = AutoregressiveTransformerDecoder(
+        num_layers=num_layers,
+
+        dim=dim,
+        heads=heads,
+        dropout=dropout,
+        activation=activation,
+        norm_first=norm_first,
 
         num_tokens=num_tokens,
         max_seq_len=max_seq_len,
@@ -106,29 +240,20 @@ if __name__ == '__main__':
     )
 
     # mandatory decoder inputs
-    seq_len = 96
-    sample_context = torch.randn(2, 320, 384)
-    sample_target = torch.randint(0, num_tokens, (2, seq_len))
+    sample_tokens = torch.randint(low=0, high=num_tokens, size=(2, 96))
+    sample_memory = torch.randn(2, 64, 384)
 
     # auto regressive wrapper forward pass
-    loss = decoder(
-        target=sample_target,
-        context=sample_context,
+    logits = autoregressive_decoder(
+        input_tokens=sample_tokens,
+        memory_logits=sample_memory,
     )
-    print(loss)
-
-    # optional generation inputs
-    from x_transformers.autoregressive_wrapper import top_k
-    optional_inputs = dict(
-        filter_logits_fn=top_k,
-        temperature=1.0,
-    )
+    print(logits.shape)
 
     # generation pass
-    sample_outputs = decoder.generate(
-        seq_len=seq_len,
-        context=sample_context,
-
-        **optional_inputs
+    autoregressive_decoder.eval()
+    sample_outputs = autoregressive_decoder.generate(
+        memory_logits=sample_memory,
     )
+    print(sample_outputs)
     print(sample_outputs.shape)
