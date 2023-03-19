cb1cad483cb5bfd9ce0d2e39ac169ab8e12031cb
IlyasMoutawwakil <ilyas.moutawwakil@student-cs.fr>
2023-03-19 01:23:12 +0100

    grouped all parameters in one dict

--- dataset/line_generation_utils.py
+++ dataset/line_generation_utils.py
@@ -1,24 +1,19 @@
 import random
-import numpy as np
 from trdg.utils import load_fonts
 from trdg.data_generator import FakeTextDataGenerator
 
-FONTS = load_fonts('fr')
-# filter out fonts names that contains "__"
-FONTS = [font for font in FONTS if "__" not in font]
-REMOVE_FONTS = [
-    "/home/ilyas/micromamba/envs/transformers-ocr/lib/python3.9/site-packages/trdg/fonts/latin/Capture_it_2.ttf",
-    "/home/ilyas/micromamba/envs/transformers-ocr/lib/python3.9/site-packages/trdg/fonts/latin/BEBAS___.ttf",
-    "/home/ilyas/micromamba/envs/transformers-ocr/lib/python3.9/site-packages/trdg/fonts/latin/SEASRN__.ttf",
-    "/home/ilyas/micromamba/envs/transformers-ocr/lib/python3.9/site-packages/trdg/fonts/latin/Capture_it.ttf",
-    "/home/ilyas/micromamba/envs/transformers-ocr/lib/python3.9/site-packages/trdg/fonts/latin/Walkway_Oblique_SemiBold.ttf",
-]
 
-FONTS = [font for font in FONTS if font not in REMOVE_FONTS]
+BAD_FONTS = [
+    "Capture_it_2.ttf",
+    "BEBAS___.ttf",
+    "SEASRN__.ttf",
+    "Capture_it.ttf",
+    "Walkway_Oblique_SemiBold.ttf",
+]
+FONTS = [font for font in load_fonts('fr') if font not in BAD_FONTS]
 
 COLORS = [
     "#000000",
-    # "#FFFFFF", # white is not a good color for text
     "#FF0000",
     "#00FF00",
     "#0000FF",
@@ -27,38 +22,36 @@ COLORS = [
     "#FF00FF",
 ]
 
-FIXED_PARAMS = dict(
-    index=0,
-    blur=1,
-    width=-1,
-    skewing_angle=2,
-    orientation=0,
-    name_format=0,
-    output_bboxes=0,
-
-    alignment=0,
-    out_dir=None,
-    extension=None,
-    word_split=False,
-    output_mask=False,
-    is_handwritten=False,
-    image_mode="RGB",
-    image_dir="",
-)
-
 
 def generate_line(text):
-    random_params = dict(
+    gen_params = dict(
+        # fixed parameters
+        index=0,
+        blur=1,
+        width=-1,
+        skewing_angle=1,
+        orientation=0,
+        name_format=0,
+        output_bboxes=0,
+        alignment=0,
+        out_dir=None,
+        extension=None,
+        word_split=False,
+        output_mask=False,
+        is_handwritten=False,
+        image_mode="RGB",
+        image_dir="",
+        
+        # random parameters
         font=random.choice(FONTS),
         text_color=random.choice(COLORS),
         stroke_fill=random.choice(COLORS),
         stroke_width=random.randint(0, 3),  # stroke width in pixels
         size=random.randint(64, 128),  # height in pixels
-        # 0: Gaussian Noise, 1: Plain white, 2: Quasicrystal, 3: Pictures
-        # 3 is not interesting for us
-        background_type=random.choice([0, 1, 2]),
+        # 0: Gaussian Noise, 1: Plain white
+        background_type=random.choice([0, 1]),
         # 0: None (Default), 1: Sine wave, 2: Cosine wave, 3:Random
-        distorsion_type=random.choice([0, 1, 2, 3]),
+        distorsion_type=random.choice([0, 3]),
         # 0: Vertical (Up and down), 1: Horizontal (Left and Right), 2: Both
         distorsion_orientation=random.choice([0, 1, 2]),
         # if true, the skewing angle is random between -skewing_angle and +skewing_angle
@@ -70,15 +63,18 @@ def generate_line(text):
         # characters level spacing in pixels
         character_spacing=random.randint(1, 10),
         # margins in pixels
-        margins=(random.randint(0, 10) for _ in range(4)),
+        margins=[random.randint(0, 10)]*4,
         # if true, tight crop the image to the text
         fit=random.choice([True, False]),
     )
 
-    image = FakeTextDataGenerator.generate(
-        text=text,
-        **random_params,
-        **FIXED_PARAMS
-    )
+    try:
+        image = FakeTextDataGenerator.generate(
+            text=text,
+            **gen_params,
+        )
+    except Exception as e:
+        # sometimes the generator fails due to a bad combination of parameters
+        return generate_line(text)
 
     return image
